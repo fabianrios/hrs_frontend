a9f86e883896d52c9e2e2224e8f3140d94405a14
fabianrios <hola@fabianrios.co>
2015-07-02 19:06:40 -0500

    organigrama

--- src/app/organigram/organigram.js
+++ src/app/organigram/organigram.js
@@ -11,15 +11,15 @@
           templateUrl: 'app/organigram/organigram.tpl.html',
           controller: 'Organigram.MainController',
             resolve: {
-              employees: function(Organigram, $stateParams){
-                return Organigram.index({id: $stateParams.id}).$promise;
+              organigram: function(Organigram, $stateParams){
+                return Organigram.show({id: $stateParams.id}).$promise;
               }
             }
         })
     })
 	
 	
-    .controller('Organigram.MainController', function($scope, $http, employees, currentUser, Auth){
+    .controller('Organigram.MainController', function($scope, $http, organigram, currentUser, HRAPI_CONF, $stateParams){
 		
 		
 	   // función de jairo para iterar
@@ -30,17 +30,189 @@
 		// 		}
 		// 	}
 		// }
+				
+		$scope.mostrar = function(e) {
+			$(e.currentTarget).parent().children("org-info").slideToggle();
+			console.log($(e.currentTarget).parent());
+		}
 		
 		$scope.user = currentUser;
 		
-		$scope.employees = employees.organigram;
-		
-		// $scope.employees.splice(0,2);
-		$scope.subordinados = $.grep($scope.employees, function(e){ return e.fther == "00002133"; });
-		$scope.elempleado = $.grep($scope.employees, function(e){ return e.pernr == "00002133"; });
-		$scope.jefes = $.grep($scope.employees, function(e){ return e.pernr == $scope.elempleado[0].fther; });
+		$scope.organigram = organigram.organigram;
+	
+		console.log($scope.organigram);
+		var m = [10, 20, 10, 20],
+		    w = 1280 - m[1] - m[3],
+			h = 1300 - m[0] - m[2],
+			i = 0,
+			url =  $stateParams.id,
+			root;
+		url = HRAPI_CONF.apiBaseUrl("/organigram/show/"+url+".json")
+		console.log("url",url);
+		var tree = d3.layout.tree()
+			.size([h, w]);
+		// var cluster = d3.layout.cluster()
+		//    .size([height, width-400]);
+		var diagonal = d3.svg.diagonal()    
+		   .projection (function(d) { return [d.y, d.x];}); 
+		var svg = d3.select("#chart").append("svg")    
+		   .attr("width",w)    
+		   .attr("height",h)    
+		   .append("g")    
+		   .attr("transform","translate(300,0)"); 
+		   d3.json(url, function(error, json){    
+		   root = json.organigram
+		   root.x0 = h / 2;
+		   root.y0 = 0;
+	   
+		  function toggleAll(d) {
+	 	    if (d.children) {
+	 	      d.children.forEach(toggleAll);
+	 	      toggle(d);
+	 	    }
+	 	  }
+	  
+		 // Initialize the display to show a few nodes.
+		  root.children.forEach(toggleAll);
+		  toggle(root.children[0]);
+	  
+	 	  update(root);
+		}); 
+	
 
+	function update(source) {
+	  var duration = d3.event && d3.event.altKey ? 5000 : 500;
+
+	  // Compute the new tree layout.
+	  var nodes = tree.nodes(root).reverse();
+
+	  // console.log(nodes);
+	  // Normalize for fixed-depth.
+	  nodes.forEach(function(d) { d.y = d.depth * 350; });
+
+	  // Update the nodes…
+	  var node = svg.selectAll("g.node")
+	      .data(nodes, function(d) { return d.id || (d.id = ++i); });
+
+	  // Enter any new nodes at the parent's previous position.
+	  var nodeEnter = node.enter().append("svg:g")
+	      .attr("class", "node")
+	      .attr("transform", function(d) { return "translate(" + source.y0 + "," + source.x0 + ")"; })
+	      .on("click", function(d) { toggle(d); update(d); });
+
+
+	  nodeEnter.append("svg:image")
+          .attr("x", function(d) { return d.children || d._children ? -55 : 15; })
+          .attr('y',-20)
+		  .attr('width', 40)
+          .attr('height', 40)
+		  .attr("xlink:href",	function(d) { return d.image; })
+	
+	  
+	  nodeEnter.append("svg:circle")
+	      .attr("class", "thelink")
+	      .attr("r", 1e-6)
+	      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+	  nodeEnter.append("svg:text")
+	      .attr("x", function(d) { return d.children || d._children ? -60 : 60; })
+	      .attr("dy", "-0.5em")
+	      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+	      .text(function(d) { return d.short_name; })
+		  .style("font-size","16px")
+	      .style("fill-opacity", 1e-6);
+  
+	  nodeEnter.append("svg:text")
+		  .attr("class", "cargo")
+	      .attr("x", function(d) { return d.children || d._children ? -60 : 60; })
+	      .attr("dy", ".4em")
+	      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+	      .text(function(d) { return d.posicion; })
+		  .style("font-size","12px")
+		  .style("fill", "#6f6f6f")
+	      .style("fill-opacity", 1e-6);
+		  
+	  nodeEnter.append("svg:text")
+		  .attr("class", "tel")
+	      .attr("x", function(d) { return d.children || d._children ? -60 : 60; })
+	      .attr("dy", "1.4em")
+	      .attr("text-anchor", function(d) { return d.children || d._children ? "end" : "start"; })
+	      .text(function(d) { return d.phone; })
+		  .style("font-size","12px")
+		  .style("fill", "#6f6f6f")
+	      .style("fill-opacity", 1e-6);
+
+	  // Transition nodes to their new position.
+	  var nodeUpdate = node.transition()
+	      .duration(duration)
+	      .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; });
+
+	  nodeUpdate.select("circle.thelink")
+	      .attr("r", 6.5)
+	      .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+
+	  nodeUpdate.selectAll("text")
+	      .style("fill-opacity", 1);
+
+	  // Transition exiting nodes to the parent's new position.
+	  var nodeExit = node.exit().transition()
+	      .duration(duration)
+	      .attr("transform", function(d) { return "translate(" + source.y + "," + source.x + ")"; })
+	      .remove();
+
+	  nodeExit.select("circle.thelink")
+	      .attr("r", 1e-6);
+
+	  nodeExit.selectAll("text")
+	      .style("fill-opacity", 1e-6);
+
+	  // Update the links…
+	  var link = svg.selectAll("path.link")
+	      .data(tree.links(nodes), function(d) { return d.target.id; });
+
+	  // Enter any new links at the parent's previous position.
+	  link.enter().insert("svg:path", "g")
+	      .attr("class", "link")
+	      .attr("d", function(d) {
+	        var o = {x: source.x0, y: source.y0};
+	        return diagonal({source: o, target: o});
+	      })
+	    .transition()
+	      .duration(duration)
+	      .attr("d", diagonal);
+
+	  // Transition links to their new position.
+	  link.transition()
+	      .duration(duration)
+	      .attr("d", diagonal);
+
+	  // Transition exiting nodes to the parent's new position.
+	  link.exit().transition()
+	      .duration(duration)
+	      .attr("d", function(d) {
+	        var o = {x: source.x, y: source.y};
+	        return diagonal({source: o, target: o});
+	      })
+	      .remove();
+
+	  // Stash the old positions for transition.
+	  nodes.forEach(function(d) {
+	    d.x0 = d.x;
+	    d.y0 = d.y;
+	  });
+	}
+
+	// Toggle children.
+	function toggle(d) {
+	  if (d.children) {
+	    d._children = d.children;
+	    d.children = null;
+	  } else {
+	    d.children = d._children;
+	    d._children = null;
+	  }
+	}
+	
 		
-		console.log($scope.subordinados, $scope.elempleado, $scope.jefes);
     });
 }());
--- src/common/resources/organigram_api.js
+++ src/common/resources/organigram_api.js
@@ -4,7 +4,7 @@
   angular.module('organigram.service', [])
 
   .factory('Organigram', function($resource, HRAPI_CONF) {
-    var url = HRAPI_CONF.apiBaseUrl('/organigram/index/:id.json');
+    var url = HRAPI_CONF.apiBaseUrl('/organigram/show/:id.json');
     return $resource(url, { id: '@id' }, {
       'index': { method: 'GET', isArray: false },
 	  'show': { method: 'GET', isArray: false }
