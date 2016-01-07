02fc619bc87448703a5d10e457f0a165905a5047
jpcontreras <jpcontreras.dev@gmail.com>
2016-01-07 16:49:44 -0500

    create new directory and html view to show all indebteness level in a table also create one angular module and controller for the html view

---
+++ src/app/indebtedness_levels/indebtedness_levels.js
@@ -0,0 +1,34 @@
+(function(){
+	'use strict';
+  
+	angular.module('indebtedness_levels', ['ui.date'])
+	.config(function($stateProvider){
+		$stateProvider
+		.state('main.views.indebtedness_levels', {
+			url 				: '/indebtedness_levels',
+			templateUrl	: 'app/indebtedness_levels/indebtedness_levels.tpl.html',
+			controller  : 'IndebtednessLevels.ListController',
+			resolve     : {}
+		})
+	})
+	.controller('IndebtednessLevels.ListController', function($rootScope, $scope){
+		$scope.payroll_date_filter = ''
+
+		if($scope.user.indebtedness_levels.length == 0){
+			$state.transitionTo('main.views.dashboard');
+  	}
+  	
+    var uniqueVals = [];
+    $.each($scope.user.indebtedness_levels, function(i, value){
+      if($.inArray(value.payroll_date, uniqueVals) === -1) uniqueVals.push(value.payroll_date);
+    });
+    $scope.payroll_dates = uniqueVals;
+
+		var uniqueVals = [];
+		$.each($scope.user.indebtedness_levels, function(i, value){
+      if($.inArray(value.payroll_concept_txt, uniqueVals) === -1) uniqueVals.push(value.payroll_concept_txt);
+    });
+    $scope.payroll_concepts = uniqueVals;
+	})
+}());
+
