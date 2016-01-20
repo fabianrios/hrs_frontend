b861ca7731c7904cd9e068197d9e7e08afa8ee6e
fabianrios <hola@fabianrios.co>
2016-01-20 08:58:19 -0500

    going over indexOf

--- src/app/profile/profile.js
+++ src/app/profile/profile.js
@@ -11,26 +11,20 @@
 		    $scope.aprobador = Employee.show({id:0, id_posicion: identification});		      
 		}
 	
-		$scope.certificatesRoute = function (ubication){
-			if(ubication == 'main.views.certificates_error' 
-			|| ubication == 'main.views.certificates_labor' 
-			|| ubication == 'main.views.certificates_vacations' 
-			|| ubication == 'main.views.certificates_payroll' 
-			|| ubication == 'main.views.certificates_income'){
-				return true;
-			}else{
-				return false;
-			}
-		}
+	  var expected = ['main.views.certificates_error','main.views.certificates_labor','main.views.certificates_vacations','main.views.certificates_payroll','main.views.certificates_income'];
+ 
+	  $scope.certificatesRoute = function (ubication){
+	      var tryout = expected.indexOf(ubication);
+	      return tryout > -1 ? true : false;
+	  }
+		
 		$scope.toggleSelection = function(valor){
 			$("#audio_btn .fa").toggleClass("fa-volume-up");
 			$("#audio_btn .fa").toggleClass("fa-volume-off");
 			if (valor){
 				$("video").prop('muted', false); //unmute
-				// console.log(valor, "unmute");
 			}else if (!valor){
 				$("video").prop('muted', true); //mute
-				// console.log(valor, "mute");
 			}
 		}
 
