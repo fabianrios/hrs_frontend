49c764463a849ecc365590ffce9136668a9fd75f
jpcontreras <jpcontreras.dev@gmail.com>
2016-01-28 17:35:40 -0500

    create module and view for loans

--- src/app/loan_records/loan_records.js
+++ src/app/loan_records/loan_records.js
@@ -1,36 +1,43 @@
 (function(){
 	'use strict';
   
-	angular.module('indebtedness_levels', ['ui.date'])
+	angular.module('loan_records', ['ui.date'])
 	.config(function($stateProvider){
 		$stateProvider
 		.state('main.views.loan_records', {
-			url 				: '/loans',
+			url 				: '/loan_records',
 			templateUrl	: 'app/loan_records/loan_records.tpl.html',
 			controller  : 'LoanRecords.ListController',
 			resolve     : {}
 		})
 	})
 	.controller('LoanRecords.ListController', ['$rootScope', '$scope', function($rootScope, $scope){
-		/*
-		$scope.payroll_date_filter = ''
 
-		if($scope.user.indebtedness_levels.length == 0){
+		$scope.date_filter = ''
+
+		if($scope.user.loan_records.length == 0){
 			$state.transitionTo('main.views.dashboard');
   	}
-  	
+
+  	$scope.getPorcentage = function(value){
+  		return '% '+parseInt(value).toLocaleString();
+  	}
+
+  	$scope.getMoneyValue = function(value){
+			return '$ '+parseInt(value).toLocaleString();
+  	}
+
     var uniqueVals = [];
-    $.each($scope.user.indebtedness_levels, function(i, value){
-      if($.inArray(value.payroll_date, uniqueVals) === -1) uniqueVals.push(value.payroll_date);
+    $.each($scope.user.loan_records, function(i, value){
+      if($.inArray(value.fpend, uniqueVals) === -1) uniqueVals.push(value.fpend);
     });
     $scope.payroll_dates = uniqueVals;
-
+		
 		var uniqueVals = [];
-		$.each($scope.user.indebtedness_levels, function(i, value){
-      if($.inArray(value.payroll_concept_txt, uniqueVals) === -1) uniqueVals.push(value.payroll_concept_txt);
+		$.each($scope.user.loan_records, function(i, value){
+      if($.inArray(value.stext, uniqueVals) === -1) uniqueVals.push(value.stext);
     });
     $scope.payroll_concepts = uniqueVals;
-    */
 	}]);
 }());
 
